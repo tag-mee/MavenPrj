e8dfae18e37e5f5b1cbf2d64cd7a4993f20c8a8e
Aditya Jha <109282841+tag-mee@users.noreply.github.com>
2025-03-12 11:57:23 +0530

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features",
 glue = "stepDefinition", 
-plugin = { "pretty","html:target/cucumber-reports" }
+plugin = { "pretty","json:target/cucumber-reports-json/Cucumber.json" }
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 
